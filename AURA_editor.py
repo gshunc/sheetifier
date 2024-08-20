4cc6bd238d62d5a82b19f8f5f40288bcf4807d29
George Harris <gsh@ad.unc.edu>
2024-08-20 10:51:28 -0500

    created base scripts

---
+++ AURA_editor.py
@@ -0,0 +1,56 @@
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+
+SAMPLE_SPREADSHEET_ID = "1rhb3NWZmw5RSQXgbmUhGXYR8O87Pga6J3waMzk3Xdqg"
+SAMPLE_RANGE_NAME = "AURA!A3:F"
+
+def main():
+  creds = None
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+  try:
+    service = build("sheets", "v4", credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = (
+        sheet.values()
+        .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
+        .execute()
+    )
+    rows = result.get("values", [])
+
+    if not rows:
+      print("No data found.")
+      return
+
+    for row in rows:
+      patient_id = row[1]
+      print(patient_id)
+      
+  except HttpError as err:
+    print(err)
+
+
+if __name__ == "__main__":
+  main()
